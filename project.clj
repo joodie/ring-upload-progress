3e6c9e952996923a1cdd93884db3b22b2499ea43
Joost Diepenmaat <joost@zeekat.nl>
2010-09-17 20:09:13 +0200

    added project file

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject ring-upload-progress "0.1.0"
+  :description "provide upload progress data in ring session"
+  :dependencies [[ring/ring-core "0.3.0-RC2"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]])
