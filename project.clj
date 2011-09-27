ebc30de4c1fe27b33993b67c419aab0240f5a4f5
Joost Diepenmaat <joost@zeekat.nl>
2011-09-27 12:09:56 +0200

    update dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,3 @@
-(defproject ring-upload-progress "0.1.0"
+(defproject ring-upload-progress "0.1.1"
   :description "provide upload progress data in ring session"
-  :dependencies [[ring/ring-core "0.3.0-RC2"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]])
+  :dependencies [[ring/ring-core "0.3.11"]])
