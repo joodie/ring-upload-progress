ef6ab0fdc1270b6f3961aadf0f347ad8804a946d
Joost Diepenmaat <joost@zeekat.nl>
2012-07-13 12:43:04 +0200

    bump version

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject ring-upload-progress "0.1.2"
+(defproject ring-upload-progress "0.1.3"
   :description "provide upload progress data in ring session"
   :dependencies [[ring/ring-core "0.3.11"]])
